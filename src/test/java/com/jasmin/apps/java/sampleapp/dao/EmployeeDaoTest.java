e65134f80ecc175af396ab238d0f900ae5febdde
Sreenivas Reddy <jasmin_corps@outlook.com>
2022-10-30 11:01:53 +0530

    New Changes

---
+++ src/test/java/com/jasmin/apps/java/sampleapp/dao/EmployeeDaoTest.java
@@ -0,0 +1,25 @@
+package com.jasmin.apps.java.sampleapp.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void insertEmployee() {
+    }
+
+    @Test
+    public void getEmployee() {
+    }
+
+    @Test
+    public void getEmployees() {
+    }
+}
