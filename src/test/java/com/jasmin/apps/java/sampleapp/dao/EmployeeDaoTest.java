2af2fbcbcc3d98382657a7bdfccb6a483631c124
Sreenivas Reddy <jasmin_corps@outlook.com>
2022-10-31 08:33:47 +0530

    New Changes

--- src/test/java/com/jasmin/apps/java/sampleapp/dao/EmployeeDaoTest.java
+++ src/test/java/com/jasmin/apps/java/sampleapp/dao/EmployeeDaoTest.java
@@ -1,25 +1,42 @@
 package com.jasmin.apps.java.sampleapp.dao;
 
+import com.jasmin.apps.java.sampleapp.beans.Employee;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class EmployeeDaoTest {
 
-    @Before
-    public void setUp() throws Exception {
+    EmployeeDao employeeDao;
+
+    public EmployeeDaoTest() {
+        employeeDao = new EmployeeDao();
     }
 
     @Test
     public void insertEmployee() {
+        Employee employee1 = new Employee(1, "Clark Kent", LocalDate.of(1975, Month.JANUARY, 01), "Superhero", 20000);
+        Employee employee2 = new Employee(2, "Bruce Wayne", LocalDate.of(1975, Month.DECEMBER, 01), "Lead Superhero", 50000);
+        employeeDao.insertEmployee(employee1);
+        employeeDao.insertEmployee(employee2);
     }
 
     @Test
     public void getEmployee() {
+        System.out.println(employeeDao.getEmployee(1));
+        System.out.println(employeeDao.getEmployee(2));
     }
 
     @Test
     public void getEmployees() {
+        List<Employee> employees = employeeDao.getEmployees();
+        for (Employee emplyee: employees) {
+            System.out.println(emplyee);
+        }
     }
 }
